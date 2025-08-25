740bc5ffdbdcb12bd9e09a7125801e0fd398afde
Devin Fink <devinfink007@gmail.com>
2025-08-25 15:58:26 -0600

    Wierd merge bug

--- RayTracer/objects.cpp
+++ RayTracer/objects.cpp
@@ -9,7 +9,7 @@ bool Sphere::IntersectRay(Ray const& ray, HitInfo& hInfo, int hitSide) const
     cyVec3f q = cyVec3f(0.0f, 0.0f, 0.0f);
 	int r = 1;
 
-	cyVec3f oc = origin - ray.p;
+	cyVec3f oc = q - ray.p;
     float a = ray.dir.Dot(ray.dir);
     float b = 2.0 * (ray.dir.Dot(ray.p - q));
     float c = ray.p.Dot(ray.p) - r * r;
